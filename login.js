b80e9ce415764f7ea7c59ef08bb410686f1e0027
Wicthor David <wicthor.silva@gmail.com>
2025-04-07 19:54:46 -0300

    ajustes finais

--- login.js
+++ login.js
@@ -4,12 +4,11 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const usuario = document.getElementById('username').value.trim();
     const senha = document.getElementById('password').value.trim();
 
-    // Recupera os dados do usuário do localStorage
-    const dadosUsuario = JSON.parse(localStorage.getItem(`usuario_${usuario}`));
+        const dadosUsuario = JSON.parse(localStorage.getItem(`usuario_${usuario}`));
 
     if (dadosUsuario && dadosUsuario.senha === senha) {
         alert('Login bem-sucedido!');
-        // Redireciona para a página principal ou dashboard
+        // Redirecionamento
         window.location.href = 'index.html';
     } else {
         alert('Usuário ou senha incorretos.');
